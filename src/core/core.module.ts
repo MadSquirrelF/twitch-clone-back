8fc8d303a5835a5d81df500001e81ae13cf4f33d
Artem Shabanov <70577403+MadSquirrelF@users.noreply.github.com>
2025-04-27 23:43:06 +0500

    feat(livekit): add livekit service, webhook, raw-body

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -11,12 +11,16 @@ import { SessionModule } from '../modules/auth/session/session.module'
 import { TotpModule } from '../modules/auth/totp/totp/totp.module'
 import { VerificationModule } from '../modules/auth/verification/verification.module'
 import { CronModule } from '../modules/cron/cron.module'
+import { LivekitModule } from '../modules/libs/livekit/livekit.module'
 import { MailModule } from '../modules/libs/mail/mail.module'
 import { StorageModule } from '../modules/libs/storage/storage.module'
+import { IngressModule } from '../modules/stream/ingress/ingress.module'
 import { StreamModule } from '../modules/stream/stream.module'
+import { WebhookModule } from '../modules/webhook/webhook.module'
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util'
 
 import { getGraphQLConfig } from './config/graphql.config'
+import { getLiveKitConfig } from './config/livekit.config'
 import { PrismaModule } from './prisma/prisma.module'
 import { RedisModule } from './redis/redis.module'
 
@@ -32,11 +36,19 @@ import { RedisModule } from './redis/redis.module'
             useFactory: getGraphQLConfig,
             inject: [ConfigService]
         }),
+        LivekitModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: getLiveKitConfig,
+            inject: [ConfigService]
+        }),
         PrismaModule,
+        WebhookModule,
         RedisModule,
+        IngressModule,
         CronModule,
         ProfileModule,
         StorageModule,
+        LivekitModule,
         AccountModule,
         MailModule,
         SessionModule,
--- src/modules/stream/stream.resolver.ts
+++ src/modules/stream/stream.resolver.ts
@@ -1,6 +1,16 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js'
+import * as Upload from 'graphql-upload/Upload.js'
 
+import { User } from '@/prisma/generated'
+import { Authorization } from '@/src/shared/decorators/auth.decorator'
+import { Authorized } from '@/src/shared/decorators/authorized.decorator'
+import { FileValidationPipe } from '@/src/shared/pipes/file-validation.pipe'
+
+import { ChangeStreamInfoInput } from './inputs/change-stream-info.input'
 import { FiltersInput } from './inputs/filters.input'
+import { GenerateStreamTokenInput } from './inputs/generate-stream-token.input'
+import { GenerateStreamTokenModel } from './models/generate-stream-token.model'
 import { StreamModel } from './models/stream.model'
 import { StreamService } from './stream.service'
 
@@ -10,5 +20,40 @@ export class StreamResolver {
     @Query(() => [StreamModel], { name: 'findAllStreams' })
     public async findAll(@Args('filters') input: FiltersInput) {
         return this.streamService.findAll(input)
+    }
+
+    @Query(() => [StreamModel], { name: 'findRandomStreams' })
+    public async findRandom() {
+        return this.streamService.findRandom()
+    }
+
+    @Authorization()
+    @Mutation(() => Boolean, { name: 'changeStreamInfo' })
+    public async changeInfo(
+        @Authorized() user: User,
+        @Args('data') input: ChangeStreamInfoInput
+    ) {
+        return this.streamService.changeInfo(user, input)
+    }
+
+    @Authorization()
+    @Mutation(() => Boolean, { name: 'changeStreamThumbnail' })
+    public async changeThumbnail(
+        @Authorized() user: User,
+        @Args('thumbnail', { type: () => GraphQLUpload }, FileValidationPipe)
+        thumbnail: Upload
+    ) {
+        return this.streamService.changeThumbnail(user, thumbnail)
+    }
+
+    @Authorization()
+    @Mutation(() => Boolean, { name: 'removeStreamThumbnail' })
+    public async removeThumbnail(@Authorized() user: User) {
+        return this.streamService.removeThumbnail(user)
+    }
+
+    @Mutation(() => GenerateStreamTokenModel, { name: 'generateStreamToken' })
+    public async generateToken(@Args('data') input: GenerateStreamTokenInput) {
+        return this.streamService.generateToken(input)
     }
 }
